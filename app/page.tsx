eff03024f3f796b07c78cd85e843ad37653f2214
ukyo7992 <f1mach99@gmail.com>
2026-08-11 23:25:25 +0900

    Day1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-const spots= [
-  
+const spots = [
   {
     id: 1,
     name: "study spot A",
@@ -7,7 +6,6 @@ const spots= [
     wifi: true,
     power: true,
   },
-
   {
     id: 2,
     name: "study spot B",
@@ -15,24 +13,28 @@ const spots= [
     wifi: false,
     power: true,
   },
-
   {
     id: 3,
     name: "study spot C",
     area: "Higashiyama",
     wifi: true,
-    power: false
-  }
+    power: false,
+  },
+];
 
-]
+export default function Home() {
+  return (
+    <main>
+      <h1>Kyoto Study Spots</h1>
 
-{
-  spots.map((spot) => (
-    <div key={spot.id}>
-      <h2>{spot.name}</h2>
-      <p>Area: {spot.area}</p>
-      <p>WiFi: {spot.wifi ? "Available" : "Not Available"}</p>
-      <p>Power: {spot.power ? "Available" : "Not Available"}</p>
-    </div>
-  ))
+      {spots.map((spot) => (
+        <div key={spot.id}>
+          <h2>{spot.name}</h2>
+          <p>Area: {spot.area}</p>
+          <p>WiFi: {spot.wifi ? "Available" : "Not Available"}</p>
+          <p>Power: {spot.power ? "Available" : "Not Available"}</p>
+        </div>
+      ))}
+    </main>
+  );
 }
